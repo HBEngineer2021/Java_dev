c7a75d6dbf61f6850b124a8b9ec26378bfbec3fe
Motoki1102 <61136190+Motoki1102@users.noreply.github.com>
2020-04-21 02:02:57 +0900

    first commit
    学習用

---
+++ src/StringExample2.java
@@ -0,0 +1,10 @@
+
+public class StringExample2 {
+    public static void main(String[] args) {
+        String message1 = "こんにちは。";
+        String message2 = "今日はいい天気ですね。";
+        String message3 = message1 + message2;
+        System.out.println(message3);
+    }
+    
+}
